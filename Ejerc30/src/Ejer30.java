bc5ba1bd519a4e9d9b9e286019b56fa4369c2de2
Alex <149847385+alekziend@users.noreply.github.com>
2025-11-04 14:32:54 +0100

    Add files via upload

---
+++ Ejerc30/src/Ejer30.java
@@ -0,0 +1,12 @@
+public class Ejer30 {
+    public static void main(String[] args) {
+        int cont = 1;
+        while (cont <= 200) {
+            if (cont % 2 == 0) {
+                System.out.println("Contador: " + cont);
+
+            }
+            cont++;
+        }
+    }
+}
