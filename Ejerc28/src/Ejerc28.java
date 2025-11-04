af092dd56a15dccf2b335adc642575643a95cf4b
Alex <149847385+alekziend@users.noreply.github.com>
2025-11-04 14:15:19 +0100

    Add files via upload

---
+++ Ejerc28/src/Ejerc28.java
@@ -0,0 +1,11 @@
+public class Ejerc28 {
+
+    public static void main() {
+        int cont = 1;
+        while (cont <= 20) {
+            System.out.println("Contador: " + cont);
+            cont++;
+        }
+
+    }
+}
