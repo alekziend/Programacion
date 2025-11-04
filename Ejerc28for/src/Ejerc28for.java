990b3eb22d4566085ef707bcfb214fed6797350e
Alex <149847385+alekziend@users.noreply.github.com>
2025-11-04 14:32:35 +0100

    Ejercicio 28 con bucle for

---
+++ Ejerc28for/src/Ejerc28for.java
@@ -0,0 +1,7 @@
+public class Ejerc28for {
+    public static void main() {
+        for (int cont = 1; cont <= 20; cont++) { // dentro del bucle declaramos variable, añadimos la condición de cierre y el contador
+            System.out.println("Esto es un bucle for " + cont);
+        }
+    }
+}
